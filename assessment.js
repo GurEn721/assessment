6a4c71dca06fb1b89bf51ddab4558da0fd797176
GurEn721 <62495603+GurEn721@users.noreply.github.com>
2020-03-24 21:39:20 +0900

    全文を自分で書いたものに変え、隠しコマンドを追加しました

--- assessment.js
+++ assessment.js
@@ -5,33 +5,36 @@ const resultDivided = document.getElementById('result-area');
 const tweetDivided = document.getElementById('tweet-area');
 
 /**
- * 指定した要素の子どもを全て除去する
- * @param {HTMLElement} element HTMLの要素
+ * 指定した子要素のすべてを削除する
+ * @param {HTMLElemnt} element HTMLの要素
  */
 function removeAllChildren(element) {
-  while (element.firstChild) { // 子どもの要素があるかぎり除去
-    element.removeChild(element.firstChild);
-  }
+    while (element.firstChild) { //子供の要素がある限り削除
+     element.removeChild(element.firstChild);
+    }
 }
 
+const cursed = 'お前こんなことしてるとか暇かよｗｗｗｗｗm9(^Д^)ﾌﾟｷﾞｬｰ'
+
 assessmentButton.onclick = () => {
-  const userName = userNameInput.value;
-  if (userName.length === 0) { // 名前が空の時は処理を終了する
-    return;
+  const userName =  userNameInput.value;
+  if (userName.length === 0) {
+      return;//関数終了のガード句
   }
-
-  // 診断結果表示エリアの作成
+  
+  
+  //診断結果表示エリアの作成
   removeAllChildren(resultDivided);
-  const header = document.createElement('h3');
+  const header = document.createElement('h3');//HTMLを足してく
   header.innerText = '診断結果';
-  resultDivided.appendChild(header);
-
+  resultDivided.appendChild(header);//appendChildで子要素にする。この場合headerの下に新しく行を入れる感じ
+  
   const paragraph = document.createElement('p');
-  const result = assessment(userName);
+  const result = assessment(userName);//診断結果取得
   paragraph.innerText = result;
   resultDivided.appendChild(paragraph);
 
-  // ツイートエリアの作成
+  //TODOツイートエリアの作成
   removeAllChildren(tweetDivided);
   const anchor = document.createElement('a');
   const hrefValue = 'https://twitter.com/intent/tweet?button_hashtag='
@@ -48,53 +51,66 @@ assessmentButton.onclick = () => {
   script.setAttribute('src', 'https://platform.twitter.com/widgets.js');
   tweetDivided.appendChild(script);
 };
-
 const answers = [
-  '{userName}のいいところは声です。{userName}の特徴的な声はみなを惹きつけ、心に残ります。',
-  '{userName}のいいところはまなざしです。{userName}に見つめられた人は、気になって仕方がないでしょう。',
-  '{userName}のいいところは情熱です。{userName}の情熱に周りの人は感化されます。',
-  '{userName}のいいところは厳しさです。{userName}の厳しさがものごとをいつも成功に導きます。',
-  '{userName}のいいところは知識です。博識な{userName}を多くの人が頼りにしています。',
-  '{userName}のいいところはユニークさです。{userName}だけのその特徴が皆を楽しくさせます。',
-  '{userName}のいいところは用心深さです。{userName}の洞察に、多くの人が助けられます。',
-  '{userName}のいいところは見た目です。内側から溢れ出る{userName}の良さに皆が気を惹かれます。',
-  '{userName}のいいところは決断力です。{userName}がする決断にいつも助けられる人がいます。',
-  '{userName}のいいところは思いやりです。{userName}に気をかけてもらった多くの人が感謝しています。',
-  '{userName}のいいところは感受性です。{userName}が感じたことに皆が共感し、わかりあうことができます。',
-  '{userName}のいいところは節度です。強引すぎない{userName}の考えに皆が感謝しています。',
-  '{userName}のいいところは好奇心です。新しいことに向かっていく{userName}の心構えが多くの人に魅力的に映ります。',
-  '{userName}のいいところは気配りです。{userName}の配慮が多くの人を救っています。',
-  '{userName}のいいところはその全てです。ありのままの{userName}自身がいいところなのです。',
-  '{userName}のいいところは自制心です。やばいと思ったときにしっかりと衝動を抑えられる{userName}が皆から評価されています。',
-  '{userName}のいいところは優しさです。あなたの優しい雰囲気や立ち居振る舞いに多くの人が癒されています。'
+'{userName}のいいところは声です。{userName}の特徴的な声は皆を惹きつけ、心に残ります。',
+'{userName}のいいところはまなざしです。{userName}に見つめられた人は、気になって仕方がないでしょう。',
+'{userName}のいいところは情熱です。{userName}の情熱に周りの人は感化されます。',
+'{userName}のいいところは厳しさです。{userName}の厳しさがものごとをいつも成功に導きます。',
+'{userName}のいいところは知識です。博識な{userName}を多くの人が頼りにしています。',
+'{userName}のいいところはユニークさです。{userName}だけのその特徴が皆を楽しくさせます。',
+'{userName}のいいところは用心深さです。{userName}の洞察に、多くの人が助けられます。',
+'{userName}のいいところは見た目です。内側から溢れ出る{userName}の良さに皆が気を惹かれます。',
+'{userName}のいいところは決断力です。{userName}がする決断にいつも助けられる人がいます。',
+'{userName}のいいところは思いやりです。{userName}に気をかけてもらった多くの人が感謝しています。',
+'{userName}のいいところは感受性です。{userName}が感じたことに皆が共感し、わかりあうことができます。',
+'{userName}のいいところは節度です。強引すぎない{userName}の考えに皆が感謝しています。',
+'{userName}のいいところは好奇心です。新しいことに向かっていく{userName}の心構えが多くの人に魅力的に映ります。',
+'{userName}のいいところは気配りです。{userName}の配慮が多くの人を救っています。',
+'{userName}のいいところはその全てです。ありのままの{userName}自身がいいところなのです。',
+'{userName}のいいところは自制心です。やばいと思ったときにしっかりと衝動を抑えられる{userName}が皆から評価されています。',
 ];
 
+
 /**
- * 名前の文字列を渡すと診断結果を返す関数
+ * なめの文字列を渡すと診断結果を返す関数
  * @param {string} userName ユーザーの名前
  * @return {string} 診断結果
  */
+
 function assessment(userName) {
-  // 全文字のコード番号を取得してそれを足し合わせる
-  let sumOfcharCode = 0;
+  if (userName = "↑↑↓↓←→←→AB") {
+    let result = cursed;
+    return result;
+} else {
+  //全文字のコード番号を取得してそれを足し合わせる
+  let sumOfCharCode = 0;
   for (let i = 0; i < userName.length; i++) {
-    sumOfcharCode = sumOfcharCode + userName.charCodeAt(i);
+      sumOfCharCode = sumOfCharCode + userName.charCodeAt(i);
   }
 
-  // 文字のコード番号の合計を回答の数で割って添字の数値を求める
-  const index = sumOfcharCode % answers.length;
+  //文字のコード番号の合計を回答の数で割って添え字の数値を求める
+  //日付を足すと一日ごとに違う結果が出るから今日の占いとかもできる
+  const index = sumOfCharCode % answers.length;
   let result = answers[index];
 
-  result = result.replace(/{userName}/g, userName);
+  result = result.replace(/\{userName\}/g, userName);
   return result;
 }
+}
+
+
+userNameInput.onkeydown = (event) => {
+    if (event.key === 'Enter') {
+      assessmentButton.onclick();
+    }
+  };
 
 // テストコード
 console.assert(
-  assessment('太郎') === '太郎のいいところは決断力です。太郎がする決断にいつも助けられる人がいます。',
-  '診断結果の文言の特定の部分を名前に置き換える処理が正しくありません。'
+    assessment('太郎') === '太郎のいいところは決断力です。太郎がする決断にいつも助けられる人がいます。',
+    '診断結果の文言の特定の部分を名前に置き換える処理が正しくありません。'
 );
 console.assert(
-  assessment('太郎') === assessment('太郎'),
-  '入力が同じ名前なら同じ診断結果を出力する処理が正しくありません。'
+    assessment('太郎') === assessment('太郎'),
+    '入力が同じ名前なら同じ診断結果を出力する処理が正しくありません。'
 );
